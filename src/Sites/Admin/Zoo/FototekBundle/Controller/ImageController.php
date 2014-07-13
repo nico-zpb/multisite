5f59c76c333715e90cbd82d1074fff46f2f1a0b1
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-07-13 07:05:56 +0200

    sauvegarde

--- src/Sites/Admin/Zoo/FototekBundle/Controller/ImageController.php
+++ src/Sites/Admin/Zoo/FototekBundle/Controller/ImageController.php
@@ -23,13 +23,16 @@ namespace Sites\Admin\Zoo\FototekBundle\Controller;
 
 use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ImageController extends Controller
 {
     public function indexAction()
     {
         $imgs = $this->getDoctrine()->getRepository("AdminZooFototekBundle:ZFImage")->findAll();
-        return $this->render("AdminZooFototekBundle:Image:index.html.twig");
+        $cats = $this->getDoctrine()->getRepository("AdminZooFototekBundle:ZFCategory")->findAll();
+        return $this->render("AdminZooFototekBundle:Image:index.html.twig",["images"=>$imgs,"categories"=>$cats]);
     }
 
     public function newAction()
@@ -52,5 +55,26 @@ class ImageController extends Controller
         $images = $this->getDoctrine()->getRepository("AdminZooFototekBundle:ZFImage")->getAllByCategory($id);
 
         return $this->render("AdminZooFototekBundle:Image:by_category.html.twig",["category"=>$cat,"images"=>$images]);
+    }
+
+    public function filterbycatAction(Request $request)
+    {
+        $csrfProvider = $this->container->get("form.csrf_provider");
+        $form = $request->request->get("filter_by_category_form");
+
+        if(!$form["_token"] || !$csrfProvider->isCsrfTokenValid("filter_by_category",$form["_token"])){
+            throw new AccessDeniedException();
+        }
+
+        $cat = $this->getDoctrine()->getRepository("AdminZooFototekBundle:ZFCategory")->find($form["category"]);
+
+        if(!$cat){
+            throw new EntityNotFoundException();
+        }
+
+        $images = $this->getDoctrine()->getRepository("AdminZooFototekBundle:ZFImage")->getAllByCategory($form["category"]);
+
+        return $this->render("AdminZooFototekBundle:Image:by_category.html.twig",["category"=>$cat,"images"=>$images]);
+
     }
 }
