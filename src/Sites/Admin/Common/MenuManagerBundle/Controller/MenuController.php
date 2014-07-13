1d09389e1b215aceea3280419a5575e93bdd46d8
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-07-13 11:54:22 +0200

    end register root menu

--- src/Sites/Admin/Common/MenuManagerBundle/Controller/MenuController.php
+++ src/Sites/Admin/Common/MenuManagerBundle/Controller/MenuController.php
@@ -21,8 +21,10 @@
 namespace Sites\Admin\Common\MenuManagerBundle\Controller;
 
 
+use Sites\Admin\Common\MenuManagerBundle\Entity\Menu;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class MenuController extends Controller
 {
@@ -35,11 +37,48 @@ class MenuController extends Controller
 
     public function newAction()
     {
-        return $this->render("AdminCommonMenuManagerBundle:Menu:new.html.twig");
+        $errors = [];
+        $menu = new Menu();
+        return $this->render("AdminCommonMenuManagerBundle:Menu:new.html.twig", ["form_errors"=>$errors,"menu"=>$menu]);
     }
 
     public function createAction(Request $request)
     {
+        $csrfProvider = $this->get("form.csrf_provider");
+        $form = $request->request->get("new_menu_form");
 
+        if(empty($form["_token"]) || !$csrfProvider->isCsrfTokenValid("new_menu", $form["_token"])){
+            throw new AccessDeniedException();
+        }
+
+        $errors = [];
+
+        if(empty($form["title"])){
+            $errors[] = "Le champs 'nom' est requis.";
+        }
+
+        if(preg_replace('/[a-z0-9_-]/','',$form["title"]) != null){
+            $errors[] = "Le champs 'nom' contient des caractères interdits.";
+        }
+
+        $test = $this->getDoctrine()->getRepository("AdminCommonMenuManagerBundle:Menu")->findByTitle($form["title"]);
+
+        if($test){
+            $errors[] = "Un menu est déjà enregistré sous ce nom.";
+        }
+
+        $menu = new Menu();
+        $menu->setTitle($form["title"]);
+
+        if($errors){
+            return $this->render("AdminCommonMenuManagerBundle:Menu:new.html.twig", ["form_errors"=>$errors,"menu"=>$menu]);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($menu);
+        $em->flush();
+
+        $this->container->get("session")->getFlashBag()->add("success", "Le menu '" . $menu->getTitle(). "' a bien été enregistré.");
+        return $this->redirect($this->generateUrl("admin_common_menu_manager_homepage"));
     }
 }
