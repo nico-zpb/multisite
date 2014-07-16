01634fac60e2c871f37912ca6b6a869c870eb8f9
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-07-16 10:48:12 +0200

    sauvegarde

--- src/Sites/Admin/Zoo/BlogBundle/Controller/ZBCategoryController.php
+++ src/Sites/Admin/Zoo/BlogBundle/Controller/ZBCategoryController.php
@@ -21,7 +21,10 @@
 namespace Sites\Admin\Zoo\BlogBundle\Controller;
 
 
+use Sites\Admin\Zoo\BlogBundle\Entity\ZBCategory;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ZBCategoryController extends Controller
 {
@@ -33,6 +36,48 @@ class ZBCategoryController extends Controller
 
     public function newAction()
     {
-        return $this->render("AdminZooBlogBundle:ZBCategory:new.html.twig");
+        $cat = new ZBCategory();
+        return $this->render("AdminZooBlogBundle:ZBCategory:new.html.twig", ["form_errors"=>[], "entity"=>$cat]);
+    }
+
+    public function createAction(Request $request)
+    {
+        $csrfProvider = $this->container->get("form.csrf_provider");
+        $form = $request->request->get("new_category_form");
+        if(!$form["_token"] || !$csrfProvider->isCsrfTokenValid("new_category",$form["_token"])){
+            throw new AccessDeniedException();
+        }
+        $errors= [];
+        if(empty($form["name"])){
+            $errors[] = "Le champs 'nom' est manquant.";
+        }
+        if(!empty($form["name"])){
+            if(preg_replace("/[a-zA-Z0-9éèêàçùëïôâ'!?, _-]/",'',$form["name"]) != ""){
+                $errors[] = "Le champs 'nom' contient des caractères interdits.";
+            }
+        }
+        if(!empty($form["slug"])){
+            if(preg_replace("/[a-z0-9-]/",'',$form["slug"]) != ""){
+                $errors[] = "Le champs 'alias' contient des caractères interdits.";
+            }
+        }
+        $form["name"] = trim(preg_replace('/\s\s+/'," ",$form["name"]));
+        $catExists = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBCategory")->findOneByName($form["name"]);
+        if($catExists){
+            $errors[] = "Il existe déjà une catégorie du même nom.";
+        }
+        $cat = new ZBCategory();
+        $cat->setName($form["name"]);
+        if(!empty($form["slug"])){
+            $cat->setSlug($form["slug"]);
+        }
+        if($errors){
+            return $this->render("AdminZooBlogBundle:ZBCategory:new.html.twig", ["form_errors"=>$errors, "entity"=>$cat]);
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($cat);
+        $em->flush();
+        $this->container->get("session")->getFlashBag()->add("success", "La catégorie " . $cat->getName() . " a bien été enregistrée.");
+        return $this->redirect($this->generateUrl("admin_zoo_blog_category_homepage"));
     }
 } 
