051df3c4b8d0f6a8b9e4c969bcf0c6e9207f2e83
Nicolas Canfrere <canfrere.nicolas@orange.fr>
2014-07-19 09:32:25 +0200

    pagination ZBPost publiés en admin part 2

--- src/Sites/Admin/Zoo/BlogBundle/Controller/DefaultController.php
+++ src/Sites/Admin/Zoo/BlogBundle/Controller/DefaultController.php
@@ -9,13 +9,14 @@ class DefaultController extends Controller
     public function indexAction($page = 1)
     {
         $categories = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBCategory")->findAllAlphaOrdered();
+        $maxPage = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBPost")->getNumPageForPublishedByDate(10);
         if($categories){
             //$draftPosts = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBPost")->findByIsDraft(true);
             $draftPosts = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBPost")->getAllDraftOrderedByDate();
             $tags = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBTag")->findAllAlphaOrdered();
             //TODO pagination des posts : http://doctrine-orm.readthedocs.org/en/latest/reference/query-builder.html#limiting-the-result
             //$publishedPosts = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBPost")->findByIsPublished(true);
-            $publishedPosts = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBPost")->getAllPublishedOrderedByDate($page, 5);
+            $publishedPosts = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBPost")->getAllPublishedOrderedByDate($page,10,$maxPage);
             $front = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBPost")->findOneByIsFront(true);
             $frontBN = $this->getDoctrine()->getRepository("AdminZooBlogBundle:ZBPost")->findOneByIsFrontBN(true);
         } else {
@@ -26,6 +27,17 @@ class DefaultController extends Controller
             $frontBN = null;
         }
 
-        return $this->render('AdminZooBlogBundle:Default:index.html.twig', ["drafts"=>$draftPosts, "published"=>$publishedPosts, "categories"=>$categories, "alaune"=>$front, "alaunebn"=>$frontBN, "tags"=>$tags]);
+        return $this->render('AdminZooBlogBundle:Default:index.html.twig',
+            [
+                "drafts"=>$draftPosts,
+                "published"=>$publishedPosts,
+                "categories"=>$categories,
+                "alaune"=>$front,
+                "alaunebn"=>$frontBN,
+                "tags"=>$tags,
+                "currentPage"=>$page,
+                "maxPage"=>$maxPage,
+            ]
+        );
     }
 }
